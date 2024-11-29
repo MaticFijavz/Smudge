e52dcfdd62d23e731c84ada9fb44d3cc878988c7
Matic Fijavz <matic.fijavz.19625@dijak.sc-celje.si>
2024-11-29 11:28:25 +0000

    Initial commit
    Initial commit for real
    Initial commit but, this isn't the actual inital commit since I've been working incorrectly on the repo. Blaming capitalism, I need dialectical materialism. Currently I have everything in one main which is erm... is fucking stupid, maybe? will change that to some cpp files and their headers.
    Application: Smudge 0.1.0
    Engine: Silver Chariot 0.1.0
    API 1.3 VULKAN

---
+++ Smudge/Smudge.cpp
@@ -0,0 +1,101 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+
+const uint32_t HEIGHT {600};
+const uint32_t WIDTH {800};
+
+GLFWwindow* window;
+VkInstance instance;
+
+    class SmudgeMain{
+    public:
+        void run(){
+            initVulkan();
+            initWindow();
+            mainLoop();
+            cleanup();
+        }
+    private:
+        
+        void initVulkan(){
+            createInstance();
+        }
+        void initWindow(){
+            glfwInit();
+                
+            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+            glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+            
+                
+            window = glfwCreateWindow(WIDTH, HEIGHT, "SILVER CHARIOT", nullptr, nullptr);
+        }
+        void mainLoop(){
+            while (!glfwWindowShouldClose(window)){
+                glfwPollEvents();
+            }
+        }
+        void cleanup(){
+            vkDestroyInstance(instance, nullptr);
+        
+            glfwDestroyWindow(window);
+            
+            glfwTerminate();
+        }
+        void createInstance(){
+        VkApplicationInfo appinfo{};
+        appinfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appinfo.pApplicationName = "Smudge";
+        appinfo.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
+        appinfo.pEngineName = "SILVER CHARIOT";
+        appinfo.engineVersion = VK_MAKE_VERSION(0, 1, 0);
+        appinfo.apiVersion = VK_API_VERSION_1_3;
+        
+        VkInstanceCreateInfo createinfo{};
+        createinfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createinfo.pApplicationInfo = &appinfo;
+        
+        uint32_t glfwExtensionCount {0};
+        const char** glfwExtensions;
+        
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        
+        createinfo.enabledExtensionCount = glfwExtensionCount;
+        createinfo.ppEnabledExtensionNames = glfwExtensions;
+        createinfo.enabledLayerCount = 0;
+        
+        VkResult result = vkCreateInstance(&createinfo, nullptr, &instance);
+        if (result != VK_SUCCESS){
+        throw std::runtime_error("failed to create an instance IODIOT IDIOT IDIOT");
+        }
+        uint32_t extensionCount {0};
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+        
+        std::cout << "available extensions:\n";
+        for (const auto& extension : extensions){
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+        }
+
+
+    };
+
+    int main(){
+    
+        SmudgeMain app;
+        try{
+            app.run();
+        }catch (const std::exception& e){
+            std::cerr << e.what() << '\n';
+            return EXIT_FAILURE;
+        }
+        return EXIT_SUCCESS;
+    }
